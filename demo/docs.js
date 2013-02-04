f94f3cecb45521455f95cfb52e55496ad116dfe3
ppxu <xyang0110@126.com>
2013-02-04 23:50:26 +0800

    update

--- demo/docs.js
+++ demo/docs.js
@@ -1,10 +1,24 @@
+!
+function($) {
 
-!function ($) {
+	$(function() {
 
-  $(function(){
+		$('.J_Toggle').click(function() {
+			$('.bs-docs-sidenav').animate({
+				'width': '0'
+			}, 500, function() {
+				$(this).hide();
+			});
 
-    
+			$('.bs-docs-sidebar h1').animate({
+				'width': '0',
+				'padding': '20px, 0'
+			}, 500, function() {
+				$(this).hide();
+			});
 
-  })
+		});
 
-}(window.jQuery)
+	});
+
+}(window.jQuery);
